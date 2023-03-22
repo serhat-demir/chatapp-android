9a2d42b0c1ef4074274ebea7dede24c703c57072
serhat-demir <serhatdmr@protonmail.com>
2023-03-22 19:21:24 +0300

    user service

---
+++ app/src/main/java/com/serhatd/chatapp/data/remote/UserService.kt
@@ -0,0 +1,19 @@
+package com.serhatd.chatapp.data.remote
+
+import com.serhatd.chatapp.data.model.ApiResponse
+import com.serhatd.chatapp.data.model.User
+import com.serhatd.chatapp.data.model.UserRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UserService {
+    @POST("users")
+    suspend fun login(
+        @Body user: UserRequest
+    ): ApiResponse<User>
+
+    @POST("users")
+    suspend fun register(
+        @Body user: UserRequest
+    ): ApiResponse<Nothing>
+}
