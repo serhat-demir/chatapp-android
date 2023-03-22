9335196138677a67abbe95a6e6227ab644051ce6
serhat-demir <serhatdmr@protonmail.com>
2023-03-22 19:24:22 +0300

    update services

--- app/src/main/java/com/serhatd/chatapp/data/remote/MessageService.kt
+++ app/src/main/java/com/serhatd/chatapp/data/remote/MessageService.kt
@@ -2,9 +2,13 @@ package com.serhatd.chatapp.data.remote
 
 import com.serhatd.chatapp.data.model.ApiResponse
 import com.serhatd.chatapp.data.model.Message
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface MessageService {
     @GET("messages")
-    suspend fun getMessages(): ApiResponse<List<Message>>
+    suspend fun getMessages(
+        @Header("X-Auth-Token") token: String
+    ): Response<ApiResponse<List<Message>>>
 }
--- app/src/main/java/com/serhatd/chatapp/data/remote/UserService.kt
+++ app/src/main/java/com/serhatd/chatapp/data/remote/UserService.kt
@@ -3,6 +3,7 @@ package com.serhatd.chatapp.data.remote
 import com.serhatd.chatapp.data.model.ApiResponse
 import com.serhatd.chatapp.data.model.User
 import com.serhatd.chatapp.data.model.UserRequest
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -10,10 +11,10 @@ interface UserService {
     @POST("users")
     suspend fun login(
         @Body user: UserRequest
-    ): ApiResponse<User>
+    ): Response<ApiResponse<User>>
 
     @POST("users")
     suspend fun register(
         @Body user: UserRequest
-    ): ApiResponse<Nothing>
+    ): Response<ApiResponse<Nothing>>
 }
