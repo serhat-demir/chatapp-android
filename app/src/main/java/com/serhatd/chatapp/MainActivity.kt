327378b09acaee9bd06cde296fe11aeca666dbe4
serhat-demir <serhatdmr@protonmail.com>
2023-03-23 14:11:46 +0300

    login fragment & internet perm % refactor

--- app/src/main/java/com/serhatd/chatapp/MainActivity.kt
+++ app/src/main/java/com/serhatd/chatapp/MainActivity.kt
@@ -8,10 +8,11 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
     }
 }
---
+++ app/src/main/java/com/serhatd/chatapp/ui/login/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.serhatd.chatapp.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.serhatd.chatapp.MainActivity
+import com.serhatd.chatapp.R
+import com.serhatd.chatapp.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tempViewModel: LoginViewModel by viewModels()
+        viewModel = tempViewModel
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        (requireActivity() as MainActivity).binding.titleSuffix = getString(R.string.title_login)
+
+        return binding.root
+    }
+}
